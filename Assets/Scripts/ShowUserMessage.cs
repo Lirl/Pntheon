aa37afad55d1ffe18ffba49a864f5f8f67b5ad43
Liron Lavon <lironlavon@gmail.com>
2018-07-23 21:39:46 +0300

    s

---
+++ Assets/Scripts/ShowUserMessage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowUserMessage : MonoBehaviour {
+
+    string message;
+    CanvasRenderer canvas;
+    private TextMeshProUGUI text;
+    float opacity = 0;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GetComponent<CanvasRenderer>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+
+        if (GameManager.Instance && GameManager.Instance.ShowMessage != null) {
+            GetComponent<Image>().enabled = true;
+            message = GameManager.Instance.ShowMessage;
+            text.text = message;
+            GameManager.Instance.ShowMessage = null;
+        } else {
+            GetComponent<Image>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
