ada3460f5ded025b6265723767469467423ba5d5
GuyGinat <guyginat4@gmail.com>
2018-07-26 13:49:27 +0300

    Asdas

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -791,13 +791,11 @@ public class Board : Photon.PunBehaviour {
         Debug.Log("Player " + (isHost ? 1 : 2) + " turn #" + TurnCounter + " YourTurn: " + isYourTurn + " isHost: " + isHost);
 
         Alert("Player " + (isHost ? 1 : 2) + " turn #" + TurnCounter);
-        var children = Hand.GetComponentsInChildren<Image>().Length;
-        if (children < 3) {
-            Debug.Log("You Have less than 3 cards in your hand");
+        if (isYourTurn) {
             DrawCard();
-        } else {
-            Debug.Log("Number of Children is " + children);
-        }
+        }      
+        
+        
         // 10 seconds turn
         // TODO: add end turn indication
         Debug.Log("Invoke EndTurn " + (isYourTurn ? "your turn" : "not your turn"));
