6376d5b63cfe97ceacc156343aef2cfe3a58f978
unknown <eranhalavy@gmail.com>
2018-07-30 17:10:29 +0300

    yomama

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
-
+    //public AudioMixer audiomix;
     public Sound[] sounds;
     public static AudioManager Instance;
+    public Slider slider;
+
     void Awake() {
         Instance = this;
         foreach (Sound s in sounds) {
@@ -20,7 +24,13 @@ public class AudioManager : MonoBehaviour {
 
     public void Play(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        float lastVolume = s.src.volume;
+        Debug.LogError(lastVolume + " last");
+        s.src.volume = s.src.volume * slider.value;
+        Debug.LogError(s.src.volume + " after change");
         s.src.Play();
+        changeVolume(s, lastVolume);
+       
         //Debug.Log("Played Sound");
     }
 
@@ -28,4 +38,19 @@ public class AudioManager : MonoBehaviour {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.src.PlayScheduled(time);
     }
+
+    public void changeVolume(Sound s, float volume) {
+         s.src.volume = volume;
+    }
+    /*
+    public void setVolumeBack (float volume) {
+        audiomix.SetFloat("volume", volume);
+        foreach (Sound s in sounds) {
+            s.src = gameObject.AddComponent<AudioSource>();
+            s.src.clip = s.clip;
+
+            s.src.volume = s.volume * volume;
+            s.src.pitch = s.pitch;
+        }
+    }*/
 }
